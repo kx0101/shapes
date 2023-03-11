536c2029b4c5389bf9391dd82b497b87e76d37aa
kx0101 <liakos.koulaxis@yahoo.com>
2023-03-11 19:55:25 +0200

    .

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+interface Area {
+    area(): number;
+}
+
+class Rectangle implements Area {
+    constructor(
+        public x: number,
+        public y: number,
+        public width: number,
+        public height: number,
+    ) { }
+
+    area(): number {
+        return this.width * this.height;
+    }
+
+    toString() {
+        return `Rectangle(${this.x}, ${this.y}): ${this.width}x${this.height}`
+    }
+}
+
+class Circle {
+    constructor(
+        public x: number,
+        public y: number,
+        public radius: number,
+    ) { }
+
+    area(): number {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+
+console.log(`${new Rectangle(0, 0, 10, 10)}`);
---
+++ src/shapes/mod.rs
@@ -0,0 +1,4 @@
+pub mod circle;
+pub mod collisions;
+pub mod rect;
+pub mod area;
